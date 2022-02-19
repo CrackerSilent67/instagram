c8e5c2b22d44762d8efe7f0b8350da93ff79e5b1
CrackerSilent67 <79468187+CrackerSilent67@users.noreply.github.com>
2022-02-19 16:48:15 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import sys, os
 
 if __name__ == "__main__":
   try:
-      #os.system("git pull")
+      os.system("git pull")
       __import__("instagram").checkin()
   except Exception as e:
     exit(str(e))
